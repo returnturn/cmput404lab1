d2332359440072ebda79358107ccdaec74f87583
Liu Hong <lhong2@ualberta.ca>
2020-01-08 18:01:24 -0700

    2

--- 1.py
+++ 1.py
@@ -4,3 +4,7 @@ print(requests.__version__)
 
 google = requests.get("http://www.google.com")
 print(google)
+
+var = requests.get("https://raw.githubusercontent.com/returnturn/cmput404lab1/master/1.py")
+
+print(var.content)
